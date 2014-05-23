0034ca1a13cb819576893b9a16e856aa3be22ddc
malpower <malpower@ymail.com>
2014-05-23 16:26:28 +0800

    first commit

---
+++ malache2.js
@@ -0,0 +1,16 @@
+/*This simple http server is made by malpower
+ * for testing web applications for mobile.
+ * This porgram is based on Node.js which is
+ * a platform from joyent, inc. It allows user
+ * building backend service programs with JS.
+ * Node.js uses Google V8 engine, it means that
+ * it will be fast.
+ * You can also share your files on http with
+ * this simple http server.Conf.js is a configure
+ * file for this simple server.Anthor named 
+ * malpower who is web programmer in Chengdu.
+ * Email: malpower@ymail.com
+ */
+
+
+
