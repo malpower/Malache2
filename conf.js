d6db92e9368b10565a1fa9f1f110e595c0a7b069
Michael Lou <malpower@ymail.com>
2014-09-10 11:08:12 +0800

    ignore conf.js

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 module.exports={maxPostSize: 1024*1024*100,
-                domains: {"127.0.0.1:8080": "sites/test"},
+                domains: {"127.0.0.1:8081": "sites/test"},
                 template: "xml",
-                port: 8080,
+                port: 8081,
                 sessionTimeout: 1000*60*10,
                 active: "ajs"};
