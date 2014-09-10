75fb26824f70b41910e1c565387d480c4de68a8e
Michael Lou <malpower@ymail.com>
2014-09-10 15:20:39 +0800

    ..

--- malache2.js
+++ malache2.js
@@ -1,5 +1,5 @@
-/*This simple http server is made by malpower
- * for testing web applications for mobile.
+/* This simple http server is made by malpower
+ * for testing web applications of mobile.
  * This porgram is based on Node.js which is
  * a platform from joyent, inc. It allows user
  * building backend service programs with JS.
